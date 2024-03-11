1fc8c0cfcd2848757f9cf2ab17bddb32dbb0638e
CadekDavid <cadek@spsejecna.cz>
2024-03-11 09:08:02 +0100

    added method selectFlower

--- src/Farm.java
+++ src/Farm.java
@@ -6,8 +6,10 @@ public class Farm {
     private double capacity = 100;
 
 
+    public Flower selectFlower (){
+
+    }
     public void plant(Flower f){
-        System.out.println("How much ha would you like to plant?");
         if (f.neededArea < capacity){
             capacity -= f.neededArea;
             field.add(f);
